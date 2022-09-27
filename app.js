8a482e20da7c9fd31b025941e6fd3b602c4a1159
david abruzzo <davidabruzzogames@gmail.com>
2022-09-27 17:13:25 +0200

    menu responsive ok

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 let image__slider = document.getElementsByClassName('image__slider');
 
-let imageAffichee = 0 ;
+let imageAffichee = 0;
 let nombreImage = image__slider.length;
 let precedent = document.querySelector('.precedent');
 let suivant = document.querySelector('.suivant');
 
-function enleverActiveImages(){
-    for( let i=0; i < nombreImage; i++){
-        image__slider [i].classList.remove('active');
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+
+
+/*ouvrir fermer menu*/
+
+toggle.addEventListener('click', function() {
+    body.classList.toggle('open');
+})
+
+/*Carousel */
+function enleverActiveImages() {
+    for (let i = 0; i < nombreImage; i++) {
+        image__slider[i].classList.remove('active');
     }
 }
 
-suivant.addEventListener('click',function(){
-    imageAffichee ++ ;
-    if(imageAffichee>=nombreImage){
-        imageAffichee=0;
+suivant.addEventListener('click', function() {
+    imageAffichee++;
+    if (imageAffichee >= nombreImage) {
+        imageAffichee = 0;
     }
     enleverActiveImages();
     image__slider[imageAffichee].classList.add('active');
 
 })
 
-precedent.addEventListener('click',function(){
-    imageAffichee--;
-    if(imageAffichee<0){
-        imageAffichee =  nombreImage-1;
+precedent.addEventListener('click', function() {
+        imageAffichee--;
+        if (imageAffichee < 0) {
+            imageAffichee = nombreImage - 1;
+        }
+        enleverActiveImages();
+        image__slider[imageAffichee].classList.add('active');
     }
-    enleverActiveImages();
-    image__slider[imageAffichee].classList.add('active');
-}
 
 )
